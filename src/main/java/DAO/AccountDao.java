5f6b3faf43a82ed21208d41e9b98877b86032935
Michael Pang <pangm1@tcnj.edu>
2024-12-08 15:10:27 +0000

    Account Test Cases Passed

--- src/main/java/DAO/AccountDao.java
+++ src/main/java/DAO/AccountDao.java
@@ -1,18 +1,48 @@
 package DAO;
 
 import Util.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import org.h2.command.Prepared;
+
 import Model.*;
 
 public class AccountDao {
 
     public static Account add(Account account) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'add'");
+        try {
+            Connection db = ConnectionUtil.getConnection();
+
+            if (get(account) != null) throw new Exception();
+
+            PreparedStatement sql = db.prepareStatement("INSERT INTO account (username, password) VALUES (?, ?);");
+            sql.setString(1, account.getUsername());
+            sql.setString(2, account.getPassword());
+            sql.executeUpdate();
+            
+            return get(account);
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     public static Account get(Account account) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'get'");
+        try {
+            Connection db = ConnectionUtil.getConnection();
+
+            PreparedStatement sql = db.prepareStatement("SELECT * FROM account WHERE username=? AND password=?;");
+            sql.setString(1, account.getUsername());
+            sql.setString(2, account.getPassword());
+
+            ResultSet result = sql.executeQuery();
+            result.next();
+            return new Account(result.getInt("account_id"), result.getString("username"), result.getString("password"));
+        } catch (Exception e) {
+            return null;
+        }
     }
     
 }
--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -2,21 +2,33 @@ package Service;
 
 import java.util.List;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import DAO.AccountDao;
 import Model.Account;
 
 public class AccountService {
+    static final ObjectMapper om = new ObjectMapper();
 
     public static Account add(String body) {
-        // TODO
-        return AccountDao.add(new Account(/* extract variables from json body */));
-
+        try {
+            Account account = om.readValue(body, Account.class);
+            System.out.println(account);
+            if (!account.getUsername().isBlank() && account.getPassword().length() > 4){
+                return AccountDao.add(account);
+            } else throw new Exception();
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     public static Account get(String body) {
-        // TODO
-        return AccountDao.get(new Account(/* extract variables from json body */));
-    }
-
-    
+        try {
+            return AccountDao.get(om.readValue(body, Account.class));
+        } catch (Exception e) {
+            return null;
+        }
+    } 
 }
